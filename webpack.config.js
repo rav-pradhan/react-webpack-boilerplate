051f8d519c34a089c17a4ae0fa7feb69045c1e9a
esoterox <ravipradhan@protonmail.com>
2019-12-28 17:31:57 +0000

    set up html-webpack-plugin and webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -17,5 +18,8 @@ module.exports = {
   },
   resolve: {
     extensions: [".js", ".jsx"]
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+      template: "./src/index.html"
+  })]
 };
