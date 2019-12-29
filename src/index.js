8907f81854f8c1aeb5d68335aa3b5f34e80da28a
esoterox <23668262+esoterox@users.noreply.github.com>
2019-12-29 17:50:46 +0000

    set up eslint

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 
-console.log(`Entry point for React Webpack boilerplate. You are currently in the ${process.env.NODE_ENV} environment`)
+console.log(`Entry point for React Webpack boilerplate. You are currently in the ${process.env.NODE_ENV} environment`);
+
+const hello = 'hello world';
+
+function sayGreeting(greeting) {
+  return greeting;
+}
+
+sayGreeting(hello);
