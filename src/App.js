ea72e1b11d025ade4348c20b58d3b14ecad3894f
esoterox <23668262+esoterox@users.noreply.github.com>
2019-12-29 18:23:18 +0000

    set up React, React-DOM and example component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+const App = () => {
+  return (
+    <div style={{textAlign: 'center', fontFamily: 'sans-serif'}}>
+      <h1>React Webpack Boilerplate</h1>
+      <p>This is one of many React Webpack boilerplates out there on the web.</p>
+    </div>
+  );
+};
+
+export default App
--- src/index.js
+++
@@ -1,2 +0,0 @@
-
-console.log(`Entry point for React Webpack boilerplate. You are currently in the ${process.env.NODE_ENV} environment`)
