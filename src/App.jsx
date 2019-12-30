adb512d2da19583e47b1f28016d0136e49c72d6e
esoterox <23668262+esoterox@users.noreply.github.com>
2019-12-30 14:13:48 +0000

    tweaks to styling and footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.scss';
 
+import Footer from './Footer';
+
 const App = () => (
-  <div className="main">
-    <h1 className="main__title">React Webpack Boilerplate</h1>
-    <p>This is one of many React Webpack boilerplates out there on the web.</p>
+  <div className="main__container">
+    <div className="main__content">
+      <h1 className="main__title">React Webpack Boilerplate</h1>
+      <p>
+        This is one of many React Webpack boilerplates out there on the web.
+      </p>
+    </div>
+    <Footer />
   </div>
 );
 
