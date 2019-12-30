ac8f297afd500eec050c13d1bb75c4e817c66e5f
esoterox <23668262+esoterox@users.noreply.github.com>
2019-12-30 15:10:32 +0000

    set up prod script with css/js minification and content hashing

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const merge = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
@@ -17,9 +16,4 @@ module.exports = merge(common, {
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
 });
