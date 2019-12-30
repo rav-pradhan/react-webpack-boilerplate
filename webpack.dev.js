1ac00e7fb24d1754f72e0ef0e2309bfe55df6e24
esoterox <23668262+esoterox@users.noreply.github.com>
2019-12-30 15:32:36 +0000

    removed output section of dev script

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,8 @@
-const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
   module: {
     rules: [
       {
