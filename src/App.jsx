c4590e72ebf2840a5b178b4750f9b74bffc9baa3
esoterox <23668262+esoterox@users.noreply.github.com>
2019-12-30 15:25:02 +0000

    added splash image for file loading demonstration

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.scss';
+import splash from './splash.jpg';
 
 import Footer from './Footer';
 
 const App = () => (
   <div className="main__container">
-    <div className="main__content">
+    <div>
       <h1 className="main__title">React Webpack Boilerplate</h1>
       <p>
         This is one of many React Webpack boilerplates out there on the web.
       </p>
+      <img src={splash} alt="splash to demonstrate file loading via webpack" style={{ width: '1024px', height: '768px' }} />
     </div>
     <Footer />
   </div>
